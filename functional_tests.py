14ea3bb448f8cd55334a7655977ef250451bf841
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-09-27 17:35:59 -0300

    Recebe novo item via requisição POST

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 import time
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common import keys
 from selenium.webdriver.common.keys import Keys
 
 
@@ -40,20 +41,27 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows),
-            "New to-do item did not appear in table",
-        )
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
         # item. Ela insere "Use peacock feathers to make a fly" (usar penas de pavão
         # para fazer um fly - Edith é bem metódica).
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Use the peacock feathres to make it fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em duas lista.
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn(
+            "2: Use peacock feathers to make a fly", [row.text for row in rows]
+        )
 
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota que o
         # site gerou um URL único para ela -- há um pequeno texto explicativo para isso.
+        self.fail("Finish the test!")
 
         # Ela acessa esse URL - sua lista de tarefas continua lá.
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
-from django.http import HttpRequest
+from django.http import HttpRequest, response
 
 from lists.views import home_page
 
@@ -8,4 +8,9 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_a_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
         self.assertTemplateUsed(response, "home.html")
