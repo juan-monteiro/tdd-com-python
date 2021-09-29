0e2889f6596869d94ebda93f3b6587c9d2b4dc73
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-09-29 17:49:31 -0300

    Corrige erro de digitação no teste funcional

--- functional_tests.py
+++ functional_tests.py
@@ -55,7 +55,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # A página é atualizada novamente e agora mostra os dois itens em duas lista.
         self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.check_for_row_in_list_table("2: Use the peacock feathres to make it fly")
 
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota que o
         # site gerou um URL único para ela -- há um pequeno texto explicativo para isso.
