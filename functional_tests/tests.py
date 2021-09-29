7e0eda098def9daa62d9e0c658fef9de8fb39805
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-09-29 18:35:42 -0300

    Testes funcionais agora usam LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,10 @@
 import time
-import unittest
+from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common import keys
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         options = webdriver.FirefoxOptions()
         options.headless = True
@@ -23,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith ouviu falar de uma nova aplicação online interessante para
         # lista de tarefas. Ela decide verificar sua homepage.
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # Ela percebe que o título da pagina e o cabeçalho mencionam listas de
         # tarefas (to-do)
@@ -62,7 +61,3 @@ class NewVisitorTest(unittest.TestCase):
         self.fail("Finish the test!")
 
         # Ela acessa esse URL - sua lista de tarefas continua lá.
-
-
-if __name__ == "__main__":
-    unittest.main()
