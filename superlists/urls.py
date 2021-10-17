d8cf2124755dd07cbc6d43255f47f52c1a352130
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-10-17 18:44:22 -0300

    separa configurações de url do app lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from lists import views
+from lists.urls import urlpatterns as lists_url
 
 urlpatterns = [
     path("", views.home_page),
-    path("lists/new", views.new_list, name="new_list"),
-    path("lists/<int:list_id>/", views.view_list, name="view_list"),
-    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
+    path("lists/", include(lists_url)),
 ]
