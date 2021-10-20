a4b1a362b2aa5f3976b78911f5caee7138cfba4f
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-10-20 17:57:25 -0300

    Move testes funcionais para arquivos separados

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,25 @@
+from .base import FunctionalTests
+from selenium.webdriver.common.keys import Keys
+
+
+class LayoutAndStylingTest(FunctionalTests):
+    def test_layout_and_styling(self):
+        # Edith acessa a página inicial
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela percebe que a caixa de entrada está elegantemente centralizada
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
+
+        # Ela inicia uma nova lista e vê que a entrada está elegante e
+        # centralizada ai também
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_table("1: testing")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+        )
