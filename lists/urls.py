82f799ceb25badccce8d306df911d103dc9d463f
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-12-19 15:43:20 -0300

    corrige path de new_list

--- lists/urls.py
+++ lists/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path("new/", views.new_list, name="new_list"),
+    path("new", views.new_list, name="new_list"),
     path("<int:list_id>/", views.view_list, name="view_list"),
 ]
