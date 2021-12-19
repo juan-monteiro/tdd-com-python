d67bad75c75d1ad48403e5229cae603ed54b3c42
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-12-19 15:27:44 -0300

    Refatora view_list para aceitar método POST

--- lists/urls.py
+++ lists/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path("", views.home_page),
-    path("new", views.new_list, name="new_list"),
+    path("new/", views.new_list, name="new_list"),
     path("<int:list_id>/", views.view_list, name="view_list"),
-    path("<int:list_id>/add_item", views.add_item, name="add_item"),
 ]
