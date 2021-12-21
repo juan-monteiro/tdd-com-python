c28189c5744df96d9f982dc3c68b2e5b4f9993ac
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-12-21 16:56:18 -0300

    Implemente validação de itens duplicados

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -30,6 +30,7 @@ class ItemValidationTest(FunctionalTests):
         self.get_item_input_box().send_keys(Keys.ENTER)
 
         # E novamente o navegador a impede
+        self.wait_for_row_in_table("1: Buy milk")
         self.wait_for(
             lambda: self.browser.find_element_by_css_selector("#id_text:invalid").text
         )
@@ -39,3 +40,22 @@ class ItemValidationTest(FunctionalTests):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_table("1: Buy milk")
         self.wait_for_row_in_table("2: Make tea")
+
+    def test_cannot_add_dublicate_items(self):
+        # Edith vai na página inicial e começa uma nova lista
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Buy wellies")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_table("1: Buy wellies")
+
+        # Ela acidentalmente tenta adicionar um item duplicado
+        self.get_item_input_box().send_keys("Buy wellies")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # Ela vê uma mensagem de erro
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector(".has-error").text,
+                "You've already got this in your list",
+            )
+        )
