ca456d4e5d9b9e3f797ba23ee4257620e46f1800
Juan Monteiro <juan.monteiro@jmonteiro.net>
2021-12-22 20:16:28 -0300

    Adiciona teste funcional para mensagem de erro

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.keys import Keys
 
 
 class ItemValidationTest(FunctionalTests):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector(".has-error")
+
     def test_cannot_add_empty_list_items(self):
         # Edith acessa a página inicial e acidentalmente tenta submeter
         # um item vazio na lista. Ela tecla Enter na caixa de entrada vazia
@@ -55,7 +58,24 @@ class ItemValidationTest(FunctionalTests):
         # Ela vê uma mensagem de erro
         self.wait_for(
             lambda: self.assertEqual(
-                self.browser.find_element_by_css_selector(".has-error").text,
+                self.get_error_element().text,
                 "You've already got this in your list",
             )
         )
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith começa uma lisca e causa um erro de validação
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Banter too thick")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_table("1: Banter too thick")
+        self.get_item_input_box().send_keys("Banter too thick")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(self.get_error_element()))
+
+        # Ela comaça a digitar no campo de entrada e o erro desaparece
+        self.get_item_input_box().send_keys("a")
+
+        # Ela fica feliz em ver o erro desaparecer
+        self.wait_for(lambda: self.assertFalse(self.get_error_element().is_displayed()))
